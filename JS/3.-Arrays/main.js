597772612b2fd9f432be0d8d278f53216cffbcf0
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2020-12-22 20:48:11 +0100

    Formating arrays

--- JS/3.-Arrays/main.js
+++ JS/3.-Arrays/main.js
@@ -4,168 +4,178 @@
     ¿Y si no sabemos exactamente cuantos valores hay?
         PUES EXISTEN LOS ARRAYS
 */
-// // let msg = "Hola Mundo";
-// // let msg2 = "¿Qué tal?";
 
-// let i = 0;
-// let n = 2;
-// /*
-//
+// Esto ya no lo tendremos más
 
-//     Existen los arrays (un conjunto de valores almacenados en una misma variable)
-// */
-// // while (i < n) {
-// //     let msg = prompt("Introduce el mensaje", i);
-// //     console.log(msg);
-// //     i++;
-// // }
-
-// // let msgs = ["Hola Mundo", "¿Qué tal?", "El mundo es cruel"];
-// // console.log(msgs);
-
-// // console.log("Hola".length);
-// // console.log("Hola"[1]);
-
-// // let str = ["H", "o", "l", "a"];
-// // console.log(str[0], str[1], str[2], str[3], str[4]);
-
-// // console.log(msgs[1]);
-
-// // i = 0;
-// // let msg = [];
-// // let continuar = true;
-// // while (continuar) {
-// //     msg[i] = prompt("Introduce el mensaje", i);
-// //     if (prompt("Quieres continuar [y] o no [n]?") === "n")
-// //         continuar = false;
-// //     i++;
-// // }
-// // console.log(msg);
+// let msg = "Hola Mundo";
+// let msg2 = "¿Qué tal?";
+// let msg3 = "Demasiados mensajes"
 
 
-// // let msg = [];
-// // let continuar = true;
-// // while (continuar) {
-// //     msg.push(prompt("Introduce el mensaje"));
-// //     if (prompt("Quieres continuar [y] o no [n]?") === "n")
-// //         continuar = false;
-// // }
-// // console.log(msg);
+/*
+    ¿Qué es un array?
+        Un array es un conjunto de valores almacenados en la misma variable
+        Estos valores se organizan en base a una posición incremental 0, 1, 2, 3...
+*/
+let msgs = ["Hola Mundo", "¿Qué tal?", "El mundo es cruel"];
+console.log("msgs:", msgs);
 
-// let messages = [1, 2, 3];
-// console.log(messages, messages.length);
-// //añadir uno al final
-// messages.push(4);
-// console.log("Push", messages.push("Hola Mundo"));
-// console.log(messages, messages.length);
-// //Eliminar uno del final
-// console.log("Pop", messages.pop());
-// console.log(messages, messages.length);
+// Un array también tiene la propiedad .length al igual que un string, puesto que funcionan de forma parecida
+console.log("Hola".length);
+console.log(msgs.length);
+// Y al igual que para determinar la posición del carácter a imprimir usábamos [i], igual para los arrays
+console.log("Hola"[1]);
+console.log(msgs[1]);
+
+let str = ["H", "o", "l", "a"];
+console.log(str[0], str[1], str[2], str[3], str[4]);
+console.log(msgs[0], msgs[1], msgs[2]);
+
+/*
+    Podríamos crear los elementos de un array en base a la entrada de un usuario
+*/
+i = 0;
+let msg = [];
+let continuar = true;
+while (continuar) {
+    msg[i] = prompt("Introduce el mensaje", i);
+    if (prompt("Quieres continuar [y] o no [n]?") === "n")
+        continuar = false;
+    i++;
+}
+console.log(msg);
+
+/*
+    Pero crear un mensaje en una posición determinada nunca es la mejor manera, para ello existe el método .push
+    el cual nos permite añadir un elemento al final del array
+*/
+
+let msg = [];
+let continuar = true;
+while (continuar) {
+    msg.push(prompt("Introduce el mensaje"));
+    if (prompt("Quieres continuar [y] o no [n]?") === "n")
+        continuar = false;
+}
+console.log(msg);
+
+let messages = [1, 2, 3];
+console.log(messages, messages.length);
+// añadir uno al final
+messages.push(4);
+console.log("Push", messages.push("Hola Mundo"));
+console.log(messages, messages.length);
+// Eliminar uno del final
+console.log("Pop", messages.pop());
+console.log(messages, messages.length);
 
 
-// //Añade un nuevo elemento al comienzo del array
-// console.log(messages.unshift(6));
-// console.log(messages, messages.length);
-// console.log("Shift", messages.shift());
-// console.log(messages, messages.length);
+// Añade un nuevo elemento al comienzo del array
+console.log(messages.unshift(6));
+// Eliminar un elemento del comienzo del array
+console.log("Shift", messages.shift());
 
 
-// //Eliminar un elemento en la posición dada
-// messages.splice(2, 2);
-// console.log(messages);
+// Eliminar un elemento en la posición dada
+messages.splice(2, 2);
 
-// //Añadir en la posición dada
-// messages.splice(2, 0, "Hola", "Mundo", "Que", "Tal");
-// console.log(messages);
+// Añadir en la posición dada
+messages.splice(2, 0, "Hola", "Mundo", "Que", "Tal");
 
 
-// /*
-//     let arr = ["Hola 1", "Mundo 2", "Prueba 3", "Prueba 4"]
-//     >> "Hola 1"
-//     >> "Mundo 2"
-//     >> "Prueba 3"
-// */
-// let arr = ["Hola 1", "Mundo 2", "Prueba 3", "Prueba 4"];
-// let length = arr.length;
-// for (let i = 0; i < length; i++) {
-//     console.log(arr[i]);
-// }
+/*
+    EJERCICIO:
+        let arr = ["Hola 1", "Mundo 2", "Prueba 3", "Prueba 4"]
+        >> "Hola 1"
+        >> "Mundo 2"
+        >> "Prueba 3"
+*/
+
+// Solución
+let arr = ["Hola 1", "Mundo 2", "Prueba 3", "Prueba 4"];
+let length = arr.length;
+for (let i = 0; i < length; i++) {
+    console.log(arr[i]);
+}
 
 
 
 // /* ------------------------- */
 
-// /*
-//     Crea una función que dado un array imprima cada uno de los elementos con su posición
+/*
+    EJERCICIO
+        Crea una función que dado un array imprima cada uno de los elementos con su posición
 
-//     ["Hola", "Mundo"]
+        ["Hola", "Mundo"]
 
-//     >> 1.- Hola
-//     >> 2.- Mundo
+        >> 1.- Hola
+        >> 2.- Mundo
 
-//     ["1.- Hola", "2.- Mundo"]
-// */
+        ["1.- Hola", "2.- Mundo"]
+*/
 
-// // function printArrayElements(array) {
-// //     const length = array.length;
+// Solución
+function printArrayElements(array) {
+    const length = array.length;
 
-// //     for (let i = 0; i < length; i++) {
-// //         console.log(i + ".-", array[i]);
-// //     }
-// // }
+    for (let i = 0; i < length; i++) {
+        console.log(i + ".-", array[i]);
+    }
+}
 
-// // printArrayElements(["Hola", "Mundo"]);
-
-
-// // printArrayElements(["HTML", "CSS", "JavaScript", "Node", "React", "DevOps"])
-
-
-// function formatArrayElements(array) {
-//     array = [...array];
-//     const length = array.length;
-
-//     for (let i = 0; i < length; i++) {
-//         array[i] = (i + ".- " + array[i]);
-//     }
-//     return array;
-// }
-
-// formatArrayElements(["Hola", "Mundo"]);
-
-// let originalArray = ["HTML", "CSS", "JavaScript", "Node", "React", "DevOps"];
-// let formatedArray = formatArrayElements(originalArray);
-
-// console.log(formatedArray);
-// console.log(originalArray);
+printArrayElements(["Hola", "Mundo"]);
+printArrayElements(["HTML", "CSS", "JavaScript", "Node", "React", "DevOps"])
 
 
 
-// let str = "Hola Mundo";
-// let str2 = [...str];
-//Destructuring Operator -> Operador desestructurante
-// console.log(str2);
+/*
+    Cuando pasamos un array como parámetro, pasamos la referencia al objeto, por lo que si modificamos el array
+    estaríamos modificando el array que nos han pasado, y eso siempre debemos de evitarlo.
 
-// let a = [1, 2, 3];
-// let b = a;
-// console.log(a === b);
+    Para crear una copia del array se puede usar el spread operator [...Array] obteniendo un nuevo array con los mismos elementos
+*/
+function formatArrayElements(array) {
+    array = [...array];
+    const length = array.length;
 
-// //true
-// //false
-// //chorizo
+    for (let i = 0; i < length; i++) {
+        array[i] = (i + ".- " + array[i]);
+    }
+    return array;
+}
+formatArrayElements(["Hola", "Mundo"]);
 
-
-// let a = [1, 2];
-// let b = [4, 5];
-
-// let c = a;
-
-// c[0] = b[0];
-
-// console.log(a, b);
-
-// [4, 2][4, 5];
+let originalArray = ["HTML", "CSS", "JavaScript", "Node", "React", "DevOps"];
+let formatedArray = formatArrayElements(originalArray);
+console.log(formatedArray);
+console.log(originalArray);
 
 
+
+
+let str = "Hola Mundo";
+let str2 = [...str];
+console.log(str2);
+
+// true puesto que son el mismo array
+let a = [1, 2, 3];
+let b = a;
+console.log(a === b);
+
+
+let a = [1, 2];
+let b = [4, 5];
+
+let c = a;
+
+c[0] = b[0];
+
+console.log(a, b);
+
+// [4, 2][4, 5]; puesto que c es el mismo array que a
+
+
+// Arrays de 2 dimensiones
 let arrayBidimensional = [0, [1, 2], 3];
 console.log(arrayBidimensional);
 console.log(arrayBidimensional[1][0]);
